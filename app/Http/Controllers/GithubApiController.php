da8d398f7a657619cca4e8c70cceccf6b546ff8d
Jonas Diel <jonasdiel@gmail.com>
2020-09-04 16:32:20 -0300

    Ajustada chamada para Curl ao invés do Guzzle

--- app/Http/Controllers/GithubApiController.php
+++ app/Http/Controllers/GithubApiController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 use function GuzzleHttp\json_decode;
 
@@ -15,12 +13,24 @@ class GithubApiController extends Controller
         $erro = null;
         if( strlen($request->usuario) ) {
             try {
-                $client = new Client();
-                $response = $client->get("https://api.github.com/users/".$request->usuario);
-                $dados = json_decode($response->getBody()->getContents());
+                $curl = curl_init();
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://api.github.com/users/".$request->usuario,
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_USERAGENT => "Required",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 0,
+                    CURLOPT_FOLLOWLOCATION => true,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "GET",
+                ));
+                $response = curl_exec($curl);
+                curl_close($curl);
+                $dados = json_decode($response);
             }
-            catch (ClientException $e) {
-                $dados[] = $e->getResponse()->getReasonPhrase();
+            catch (Exception $e) {
+                $dados[] = $e->getMessage();
             }
 
         }
